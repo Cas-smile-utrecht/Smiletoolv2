e48a98c23d16a2524975e98d44ae3fb8e4af2c98
Cas-smile-utrecht <cas@smile-utrecht.nl>
2025-07-10 13:41:02 +0200

    changed read csv function.
    Added support voor " in item discription

--- smiletool_v2.py
+++ smiletool_v2.py
@@ -10,7 +10,14 @@ import streamlit as st
 # Functie om CSV-bestanden in te lezen
 def read_csv(file):
     try:
-        df = pd.read_csv(file, sep=None, engine='python', on_bad_lines='skip')
+        df = pd.read_csv(
+            file,
+            sep=None,
+            engine='python',
+            on_bad_lines='skip',
+            quotechar='"',
+            quoting=csv.QUOTE_MINIMAL
+        )
         st.write(f"Succesvol ingelezen: {file.name}")
         return df
     except Exception as e:
